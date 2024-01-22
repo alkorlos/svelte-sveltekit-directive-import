4ab250ed743519ab95843f1acfff2fa750871875
Dmitry Statsenko <alkor.sx@gmail.com>
2024-01-22 19:51:44 +0300

    sveltekit test-directive-import

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<div class="test-directive-import"></div>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    @import "./test-directive-import.css";
+</style>
